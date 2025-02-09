a4f11c750b342e589a563b8be96cc09f77b7c703
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-02-08 22:04:43 -0300

    Serialização JSON

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,18 +1,23 @@
 using APICatalogo.context;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(option =>
+{
+    option.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 string mysqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddDbContext<AppDbContext>(options => 
-    options.UseMySql(mysqlConnection, 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mysqlConnection,
         ServerVersion.AutoDetect(mysqlConnection)));
 
 var app = builder.Build();

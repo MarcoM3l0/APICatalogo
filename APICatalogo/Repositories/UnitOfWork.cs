1894860910bbc3608e8e25d734f78c9f9a123ad4
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-05-29 16:15:24 -0300

    Implementando a class concreta do padrão Unit Of Work

---
+++ APICatalogo/Repositories/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using APICatalogo.context;
+
+namespace APICatalogo.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly IProdutoRepository _produtosRepo;
+    private readonly ICategoriaRepository _categoriasRepo;
+    
+    public readonly AppDbContext _context;
+
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IProdutoRepository Produtos
+    {
+        get
+        {
+            return _produtosRepo ?? new ProdutoRepository(_context);
+        }
+    }
+
+    public ICategoriaRepository Categorias
+    {
+        get
+        {
+            return _categoriasRepo ?? new CategoriaRepository(_context);
+        }
+    }
+
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}

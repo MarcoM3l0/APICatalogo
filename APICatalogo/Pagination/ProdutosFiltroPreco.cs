ef62440ac3fd9e891b907214a5c9e775f91a3c6d
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-06 22:13:29 -0300

    Adicionando ProdutosFiltroPreco para permitir filtragem de produtos por preço e critério (menor, maior, igual).

---
+++ APICatalogo/Pagination/ProdutosFiltroPreco.cs
@@ -0,0 +1,7 @@
+﻿namespace APICatalogo.Pagination;
+
+public class ProdutosFiltroPreco : QueryStringParameters
+{
+    public decimal? Preco;
+    public string? PrecoCriterio; // Exemplo: "menor", "maior", "igual"
+}

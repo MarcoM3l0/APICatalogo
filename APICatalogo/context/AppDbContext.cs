2ae6352522cd9ded05d0ce6b6be54e760b49844f
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-18 14:35:34 -0300

    Correção

--- APICatalogo/context/AppDbContext.cs
+++ APICatalogo/context/AppDbContext.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.context;
 
-public class AppDbContext : IdentityDbContext<IdentityUser>
+public class AppDbContext : IdentityDbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {

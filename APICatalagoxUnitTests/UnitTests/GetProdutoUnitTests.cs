baf082316e24a31bc73a6c64bc990b78b12bbb5e
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-08-05 17:07:59 -0300

    Testes Unitarios Produtos: Testando os métodos Get

--- APICatalagoxUnitTests/UnitTests/GetProdutoUnitTests.cs
+++ APICatalagoxUnitTests/UnitTests/GetProdutoUnitTests.cs
@@ -1,4 +1,6 @@
 ﻿using APICatalogo.Controllers;
+using APICatalogo.DTOs;
+using APICatalogo.Pagination;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +12,17 @@ public class GetProdutoUnitTests : IClassFixture<ProdutosUnitTestController>
     public GetProdutoUnitTests(ProdutosUnitTestController controller)
     {
         _controller = new ProdutosController(controller.repository, controller.logger, controller.mapper);
+    }
+
+    [Fact]
+    public async Task GetProdutos_Returns_ListOfProdutoDTO()
+    {
+        // Act
+        var data = await _controller.Get();
+        // Assert
+        data.Result.Should().BeOfType<OkObjectResult>() // Verifica se o resultado é do tipo OkObjectResult
+            .Which.Value.Should().BeAssignableTo<IEnumerable<ProdutoDTO>>() //Verifica se o valor do OkObjectResult é atribuído ao tipo IEnumerable<ProdutoDTO>
+            .And.NotBeNull(); // Verifica se o valor não é nulo
     }
 
     [Fact]
@@ -28,5 +41,43 @@ public class GetProdutoUnitTests : IClassFixture<ProdutosUnitTestController>
         //Assert(FluentAssertions)
         data.Result.Should().BeOfType<OkObjectResult>() // Verifica se o resultado é do tipo OkObjectResult
             .Which.StatusCode.Should().Be(200); // Verifica se o status code do OkObjectResult é 200
+    }
+
+    [Fact]
+    public async Task GetProdutoByID_Returns_NotFoundResult()
+    {
+        // Arrange
+        var ProdutoId = 1; // ID que não existe
+
+        // Act
+        var data = await _controller.Get(ProdutoId);
+
+        // Assert
+        data.Result.Should().BeOfType<NotFoundObjectResult>() // Verifica se o resultado é do tipo NotFoundObjectResult
+            .Which.StatusCode.Should().Be(404); // Verifica se o status code do NotFoundObjectResult é 404
+    }
+
+    [Fact]
+    public async Task GetProdutoByCategoria_OkResult()
+    {
+        // Arrange
+        var categoria = 1;
+        // Act
+        var data = await _controller.GetProdutosPorCategoria(categoria);
+        // Assert
+        data.Result.Should().BeOfType<OkObjectResult>() // Verifica se o resultado é do tipo OkObjectResult
+            .Which.StatusCode.Should().Be(200); // Verifica se o status code do OkObjectResult é 200
+    }
+
+    [Fact]
+    public async Task GetProdutoByCategoria_NotFoundResult()
+    {
+        // Arrange
+        var categoriaID = 99999; // Categoria que não existe
+        // Act
+        var data = await _controller.GetProdutosPorCategoria(categoriaID);
+        // Assert
+        data.Result.Should().BeOfType<NotFoundObjectResult>() // Verifica se o resultado é do tipo NotFoundObjectResult
+            .Which.StatusCode.Should().Be(404); // Verifica se o status code do NotFoundObjectResult é 404
     }
 }

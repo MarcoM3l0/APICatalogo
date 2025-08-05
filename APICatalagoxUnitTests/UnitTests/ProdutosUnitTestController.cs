0bc3c364829939924d7748b73e19383d6d753968
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-08-05 15:54:17 -0300

    Configurando o projeto de testes com xUnit

---
+++ APICatalagoxUnitTests/UnitTests/ProdutosUnitTestController.cs
@@ -0,0 +1,34 @@
+﻿using APICatalogo.context;
+using APICatalogo.DTOs.Mappings;
+using APICatalogo.Repositories;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalagoxUnitTests.UnitTests;
+internal class ProdutosUnitTestController
+{
+    public IUnitOfWork repository;
+    public IMapper mapper;
+    public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+
+    public static string connectionString = "Server=localhost;Database=CatalogoDB;Uid=root;PWD=123456";
+
+    static ProdutosUnitTestController()
+    {
+        dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+            .Options;
+    }
+
+    public ProdutosUnitTestController()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new ProdutoDTOMappingProfile());
+        });
+
+        mapper = config.CreateMapper();
+        var context = new AppDbContext(dbContextOptions);
+        repository = new UnitOfWork(context);
+    }
+}
